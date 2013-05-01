08d75af77e63f09e4487dab5b003ed118894b0c8
David <wyliang0790@gmail.com>
2013-04-30 22:08:36 -0700

    Solved problem 1

---
+++ src/Problem2.java
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: wyliang
+ * Date: 4/30/13
+ * Time: 10:02 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Problem2 {
+    public static void main(String[] args)
+    {
+
+    }
+}
